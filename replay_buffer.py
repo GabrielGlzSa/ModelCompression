e3e50c5d31034152efe29b254c240b7482ab5dfe
gabrielglzsa <gabrielglzsa@github.com>
2022-02-07 14:20:19 -0600

    Added replay_buffer to save states and actions.

---
+++ replay_buffer.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class ReplayBuffer(object):
+    def __init__(self, size):
+        self._storage = []
+        self._maxsize = size
+        self._next_idx = 0
+
+    def __len__(self):
+        return len(self._storage)
+
+    def add(self, s, a, r, s_next, done):
+
+        data = (s, a, r, s_next, done)
+        if self._next_idx >= len(self._storage):
+            self._storage.append(data)
+        else:
+            self._storage[self._next_idx] = data
+
+        self._next_idx = (self._next_idx+1) % self._maxsize
+
+
+    def sample(self, batch_size):
+        batch = np.random.choice(range(len(self._storage)), batch_size, replace=False)
+        s, a, r, s_next, done = [], [], [], [], []
+        for i in batch:
+            s_temp, a_temp, r_temp, sn_temp, done_temp = self._storage[i]
+            s.append(s_temp)
+            a.append(a_temp)
+            s_next.append(sn_temp)
+            done.append(done_temp)
+
+        return (np.array(s),
+                np.array(a),
+                np.array(r),
+                np.array(s_next),
+                np.array(done))
