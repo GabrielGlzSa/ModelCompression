fb5091ac13aae90aec666f5696d46c5a0ca77c8d
gabrielglzsa <gabriel_glzsahagun@hotmail.com>
2023-03-24 14:04:42 -0600

    Test all compressors using training set.

--- test_compressors.py
+++ test_compressors.py
@@ -7,13 +7,16 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from functools import partial
 
-from CompressionLibrary.agent_evaluators import make_env_imagenet, evaluate_agents
+from CompressionLibrary.agent_evaluators import make_env_adadeep, evaluate_agents
 from CompressionLibrary.reinforcement_models import DuelingDQNAgent
 
 
-agents_names = map(lambda x: 'DuelingDQN_'+x, ['mnist-kmnist', 'fashion_mnist-kmnist', 'mnist-fashion_mnist'])
-dataset_name= 'fashion_mnist'
+agents_names = list(map(lambda x: 'LeNet_DDDQN_MKII_'+x, ['fashion_mnist','kmnist', 'mnist', 'fashion_mnist-kmnist', 'fashion_mnist-mnist','kmnist-mnist']))
+agents_names = agents_names + ['LeNet_DDDQN_MKIII_generalist_fashion_mnist-kmnist-mnist']
+
+dataset_names = ['fashion_mnist','kmnist', 'mnist']
 run_id = datetime.now().strftime('%Y-%m-%d-%H-%M%S-') + str(uuid4())
 
 try:
@@ -31,14 +34,18 @@ except:
   strategy = tf.distribute.OneDeviceStrategy(device="/gpu:0")
 
 strategy = None
-data_path = './data'
+data_path = './data/'
   
+log_name = 'test_agents_training_ds_12k'
+test_filename = data_path + 'stats/DDDQN_MKII_{}_testing.csv'.format(log_name)
+agents_path = data_path+'agents/DDDQN/checkpoints/'
+
 if strategy:
     print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
 
 logging.basicConfig(level=logging.DEBUG, handlers=[
-    logging.FileHandler(f'/home/A00806415/DCC/ModelCompression/data/ModelCompression_DDQN_tests_{dataset_name}.log', 'w+')],
+    logging.FileHandler(data_path + f'logs/{log_name}.log', 'w+')],
     format='%(asctime)s -%(levelname)s - %(funcName)s -  %(message)s')
 logging.root.setLevel(logging.DEBUG)
 
@@ -51,26 +58,20 @@ next_state = 'layer_output'
 
 epsilon_start_value = 0.9
 verbose = 0
-eval_n_samples = 10
-n_samples_mode = -1
+eval_n_samples = 4
+n_samples_mode = 12000
 tuning_batch_size = 256
 tuning_epochs = 30
 
 layer_name_list = ['conv2d_1',  'dense', 'dense_1']
 
-def dataset_preprocessing(img, label):
-    img = tf.cast(img, tf.float32)
-    img = img/255.0
-    return img, label
 
-def load_weigths_into_target_network(agent, target_network):
-    """ assign target_network.weights variables to their respective agent.weights values. """
-    for i in range(len(agent.model.layers)):
-        target_network.model.layers[i].set_weights(
-            agent.model.layers[i].get_weights())
+def calculate_reward(stats: dict) -> float:
+   return 1 - (stats['weights_after']/stats['weights_before']) + stats['accuracy_after'] - 0.9 * stats['accuracy_before']
 
-def create_model():
-    checkpoint_path = f"./data/models/lenet_{dataset_name}/cp.ckpt"
+
+def create_model(dataset_name, train_ds, valid_ds):
+    checkpoint_path = data_path+ f"models/lenet_{dataset_name}/cp.ckpt"
     optimizer = tf.keras.optimizers.Adam(1e-5)
     loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
     train_metric = tf.keras.metrics.SparseCategoricalAccuracy()
@@ -92,17 +93,21 @@ def create_model():
         model.load_weights(checkpoint_path).expect_partial()
     except:
         cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, monitor='val_loss', save_best_only=True,
-                                                save_weights_only=True,
-                                                verbose=1)
+                                                 save_weights_only=True,
+                                                 verbose=1)
         model.fit(train_ds,
-        epochs=3000,
-        validation_data=valid_ds,
-        callbacks=[cp_callback])
+          epochs=3000,
+          validation_data=valid_ds,
+          callbacks=[cp_callback])
 
     return model       
 
+def dataset_preprocessing(img, label):
+    img = tf.cast(img, tf.float32)
+    img = img/255.0
+    return img, label
 
-def load_dataset(batch_size=128):
+def load_dataset(dataset_name, batch_size=128):
     splits, info = tfds.load(dataset_name, as_supervised=True, with_info=True, shuffle_files=True, 
                                 split=['train[:80%]', 'train[80%:]','test'])
 
@@ -120,10 +125,17 @@ def load_dataset(batch_size=128):
 
     return train_ds, valid_ds, test_ds, input_shape, num_classes
 
-train_ds, valid_ds, test_ds, input_shape, _ = load_dataset(tuning_batch_size)
+
 input_shape = (28,28,1)
-env = make_env_imagenet(
-            create_model=create_model, 
+
+def create_environments(dataset_names):
+    environments = []
+    for dataset in dataset_names:
+        train_ds, valid_ds, test_ds, input_shape, _ = load_dataset(dataset, tuning_batch_size)
+        new_func = partial(create_model, dataset_name=dataset, train_ds=train_ds, valid_ds=valid_ds)
+        env = make_env_adadeep(
+            create_model=new_func, 
+            reward_func=calculate_reward,
             train_ds=train_ds, 
             valid_ds=valid_ds, 
             test_ds=test_ds, 
@@ -133,14 +145,23 @@ env = make_env_imagenet(
             tuning_batch_size=tuning_batch_size, 
             tuning_epochs=tuning_epochs, 
             verbose=verbose, 
-            tuning_mode='layer', 
+            tuning_mode='layer',
+            get_state_from='train',
             current_state_source=current_state, 
             next_state_source=next_state, 
             strategy=strategy, 
             model_path=data_path)
-conv_shape, dense_shape = env.observation_space()
-conv_n_actions, fc_n_actions = env.action_space()
 
+        environments.append(env)
+
+    return environments
+
+envs = create_environments(dataset_names)
+envs[0].model.summary()
+print(f'{len(envs)} envs')
+
+conv_shape, dense_shape = envs[0].observation_space()
+conv_n_actions, fc_n_actions = envs[0].action_space()
     
 fc_agent = DuelingDQNAgent(name="ddqn_agent_fc", state_shape=dense_shape,
                     n_actions=fc_n_actions, epsilon=epsilon_start_value, layer_type='fc')
@@ -148,12 +169,14 @@ fc_agent = DuelingDQNAgent(name="ddqn_agent_fc", state_shape=dense_shape,
 conv_agent = DuelingDQNAgent(
     name="ddqn_agent_conv", state_shape=conv_shape, n_actions=conv_n_actions, epsilon=epsilon_start_value, layer_type='cnn')
 
-for agent_name in agents_names:
-    print(dataset_name, agent_name)
-    conv_agent.model.load_weights(
-        data_path+'/checkpoints/{}_my_checkpoint_conv_agent'.format(agent_name))
-    fc_agent.model.load_weights(
-        data_path+'/checkpoints/{}_my_checkpoint_fc_agent'.format(agent_name))
+iterations = len(dataset_names) * len(agents_names)
+with tqdm(total=iterations) as t:
+    for idx, dataset_name in enumerate(dataset_names):
+        env = envs[idx]
 
-    rw, acc, weights = evaluate_agents(env, conv_agent, fc_agent,run_id=run_id,test_number=agent_name, dataset_name=dataset_name,save_name=data_path+'/DuelingDQN_test_generalist.csv', n_games=eval_n_samples)    
+        for agent_name in agents_names:
+            # print(dataset_name, agent_name)
+            conv_agent.model.load_weights(agents_path+agent_name+'_conv_agent.ckpt')
+            fc_agent.model.load_weights(agents_path+agent_name+'_fc_agent.ckpt')
 
+            rw, acc, weights = evaluate_agents(env, conv_agent, fc_agent,run_id=run_id,test_number=agent_name, dataset_name=dataset_name, agent_name=agent_name,save_name=test_filename, n_games=eval_n_samples)    
