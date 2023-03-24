70f3c5f16ac753f87ce0dbe977a36763865c189c
gabarielglzsa <gabriel_glzsahagun@hotmail.com>
2023-03-24 13:57:12 -0600

    Added untested prioritized replay.

--- CompressionLibrary/replay_buffer.py
+++ CompressionLibrary/replay_buffer.py
@@ -170,3 +170,124 @@ class ReplayBufferMultipleDatasets(object):
                 tf.ragged.stack(s_next),
                 np.array(done))
 
+
+class PrioritizedReplayBufferMultipleDatasets(object):
+    def __init__(self, size:int, alpha:int, dataset_names:list):
+        
+        self.dataset_names = dataset_names
+
+        self._maxsize = size
+        self.alpha = alpha
+
+        # Dictionary to get the index of each dataset.
+        self.dataset_dict = dict(zip(dataset_names,range(len(self.dataset_names))))
+
+        # Create variables to store data.
+        self.reset()
+
+
+    def __len__(self):
+        return np.sum(list(map(lambda x: len(self._states[x]), self.dataset_names)))
+        
+    def reset(self):
+        self._states = dict(zip(self.dataset_names, map(lambda x: [], self.dataset_names)))
+        self._next_states = dict(zip(self.dataset_names, map(lambda x: [], self.dataset_names)))
+
+        num_datasets = len(self.dataset_names)
+        self._actions = np.zeros((num_datasets, self._maxsize), dtype=np.int32)
+        self._rewards = np.zeros((num_datasets, self._maxsize), dtype=np.float32)
+        self._done = np.full((num_datasets, self._maxsize), dtype=np.bool8)
+
+        self._td_error = np.zeros((num_datasets, self._maxsize), dtype=np.float32)
+        self._next_idx = np.zeros(num_datasets, dtype=np.int32)
+        self._max_saved = np.zeros(num_datasets, dtype=np.int32)
+        
+    def add(self, s, a, rw, done, td_error, dataset_name):
+
+        dataset_index = self.dataset_dict[dataset_name]
+        
+        
+        self._states[dataset_name].append(s)
+        self._actions[dataset_index,self._next_idx[dataset_index]] = a
+        self._rewards[dataset_index,self._next_idx[dataset_index]] = rw
+        self._done[dataset_index,self._next_idx[dataset_index]] = done
+        self._td_error[dataset_index,self._next_idx[dataset_index]] = td_error
+
+        self._next_idx[dataset_index] = (self._next_idx[dataset_index]+1) % self._maxsize
+        self._max_saved[dataset_index] = max(self._max_saved[dataset_index], self._next_idx[dataset_index])
+
+        self.last_sampled = None
+ 
+
+    def update_td_error(self, td_errors):
+
+        batch_start = 0
+        for dataset_idx, indexes in enumerate(self.last_sampled):
+            batch_size = len(indexes)
+            self._td_error[dataset_idx, indexes] = td_errors[batch_start:batch_start+batch_size]
+            batch_start = batch_size
+
+
+
+    def sample(self, batch_size):
+        # Empty list for states
+        s = []
+        actions = []
+        rewards = []
+        s_next = []
+        done = []
+
+        num_datasets = len(self.dataset_names)
+        batch_counter = 0
+        recommended_batch_size = batch_size//num_datasets
+
+        self.last_sampled = []
+
+        for dataset_name in self._states.keys():
+            dataset_idx = self.dataset_dict[dataset_name]
+            max_dataset = self._max_saved[dataset_idx]
+            td_error = self._td_error[dataset_idx, :max_dataset]
+            td_error_sorted_idx = np.argsort(td_error)
+
+            # Add the samples from the dataset that were used to train the network.
+            self.last_sampled.append(td_error_sorted_idx)
+
+            probabilities = np.empty_like(td_error)
+            probabilities[td_error_sorted_idx] = list(range(1, max_dataset+1)) 
+            probabilities = 1/probabilities
+            # Use alpha to determine how much priorization.
+            probabilities = np.power(probabilities, self.alpha)
+            # Normalize to obtain probabilities
+            probabilities = probabilities / np.sum(probabilities)
+
+            if batch_counter + recommended_batch_size > batch_size:
+                recommended_batch_size = batch_size - batch_counter
+
+            if recommended_batch_size < len(self._states[dataset_name]):
+                batch = np.random.choice(len(self._states[dataset_name]), recommended_batch_size, replace=False, p=probabilities)
+                batch_counter += recommended_batch_size
+            else:
+                num_storage_sampes = len(self._states[dataset_name])
+                batch = np.random.choice(len(self._states[dataset_name]), num_storage_sampes, replace=False, p=probabilities)
+                batch_counter += num_storage_sampes
+
+
+            for batch_element in batch:
+                # Remove  dimensions of size 1 so that it can be stacked.
+                s.append(tf.squeeze(self._states[dataset_name][batch_element]))
+                s_next.append(tf.squeeze(self._states[dataset_name][batch_element]))
+
+            dataset_index = self.dataset_dict[dataset_name]
+            actions.extend(self._actions[dataset_index][batch])
+            rewards.extend(self._rewards[dataset_index][batch])
+            done.extend(self._done[dataset_index][batch])
+
+
+        # Stack feature maps and add depth of 1.
+        s = tf.expand_dims(tf.ragged.stack(s), axis=-1)
+        s_next = tf.expand_dims(tf.ragged.stack(s_next), axis=-1)
+        return (s.to_tensor(),
+                tf.convert_to_tensor(actions, dtype=tf.int32),
+                tf.convert_to_tensor(rewards, dtype=tf.float32),
+                s_next.to_tensor(),
+                tf.convert_to_tensor(done, dtype=tf.int32))
