55bfb72996b7c7a8054ce8bd298aebe81d7c84e1
gabrielglzsa <gabriel_glzsahagun@hotmail.com>
2023-05-25 16:37:35 -0500

    Added MnasNet-like reward function.

--- CompressionLibrary/reward_functions.py
+++ CompressionLibrary/reward_functions.py
@@ -6,3 +6,6 @@ def reward_1minus1(stats: dict) -> float:
 
 def reward_original(stats: dict) -> float:
    return 1 - (stats['weights_after']/stats['weights_before']) +  (stats['accuracy_after'] - 0.9 * stats['accuracy_before'])
+
+def reward_MnasNet(stats: dict) -> float:
+   return stats['accuracy_after'] * (1 - (stats['weights_after']/stats['weights_before']))
