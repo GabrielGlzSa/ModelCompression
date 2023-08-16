f0b28c659355146c3d18b3bf4d95d2276becaf66
gabrielglzsa <gabriel_glzsahagun@hotmail.com>
2023-08-16 13:12:49 -0500

    Added new reward function.

--- CompressionLibrary/reward_functions.py
+++ CompressionLibrary/reward_functions.py
@@ -9,3 +9,6 @@ def reward_original(stats: dict) -> float:
 
 def reward_MnasNet(stats: dict) -> float:
    return stats['accuracy_after'] * (1 - (stats['weights_after']/stats['weights_before']))
+
+def reward_MnasNet_penalty(stats: dict) -> float:
+   return stats['accuracy_after'] * (1 - (stats['weights_after']/stats['weights_before'])) if stats['accuracy_after'] > 0.9 * stats['accuracy_before'] else 0.0
